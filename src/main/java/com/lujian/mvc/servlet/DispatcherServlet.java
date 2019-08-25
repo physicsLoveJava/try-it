2fbfb4886a1980e4d177a649c4261bd299f414be
编城 <lj167323@alibaba-inc.com>
2019-08-25 17:49:58 +0800

    update version

--- src/main/java/com/lujian/mvc/servlet/DispatcherServlet.java
+++ src/main/java/com/lujian/mvc/servlet/DispatcherServlet.java
@@ -15,6 +15,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -27,6 +29,7 @@ import com.lujian.mvc.annotation.Controller;
 import com.lujian.mvc.annotation.RequestMapping;
 import com.lujian.mvc.annotation.RequestParam;
 import com.lujian.mvc.annotation.Service;
+import lombok.Data;
 
 /**
  * @author lj167323@alibaba-inc.com
@@ -39,7 +42,7 @@ public class DispatcherServlet extends HttpServlet {
 
     private Map<String, Object> ioc = new HashMap<>();
 
-    private Map<String, Object> handlerMapping = new HashMap<>();
+    private List<Handler> handlerMapping = new ArrayList<>();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -57,47 +60,69 @@ public class DispatcherServlet extends HttpServlet {
 
     private void doDispatcher(HttpServletRequest req, HttpServletResponse resp)
         throws IOException, InvocationTargetException, IllegalAccessException {
-        String url = req.getRequestURI();
-        String contextPath = req.getContextPath();
-        url = url.replace(contextPath, "").replaceAll("/+", "/");
-        if (!handlerMapping.containsKey(url)) {
+        Handler handler = getHandler(req);
+        if (handler == null) {
             resp.getWriter().write("404 Not Found");
             return;
         }
-        Map<String, String[]> params = req.getParameterMap();
-        Method method = (Method)this.handlerMapping.get(url);
-        Class<?>[] parameterTypes = method.getParameterTypes();
-        Map<String, String[]> parameterMap = req.getParameterMap();
-        Object[] paramValues = new Object[parameterTypes.length];
+        Class<?>[] paramTypes = handler.getParamTypes();
 
-        for (int i = 0; i < parameterTypes.length; i++) {
-            Class<?> parameterType = parameterTypes[i];
-            if (parameterType == HttpServletRequest.class) {
-                paramValues[i] = req;
+        Object[] paramValues = new Object[paramTypes.length];
+
+        Map<String, String[]> params = req.getParameterMap();
+
+        for (Entry<String, String[]> param : params.entrySet()) {
+            String value = Arrays.toString(param.getValue()).replaceAll("\\[|\\]", "").replaceAll("\\s", ",");
+            if (!handler.paramIndexMapping.containsKey(param.getKey())) {
                 continue;
-            }else if(parameterType == HttpServletResponse.class) {
-                paramValues[i] = resp;
-                continue;
-            }else if(parameterType == String.class) {
-                Annotation[][] pa = method.getParameterAnnotations();
-                for (int j = 0; j < pa.length; j++) {
-                    for (Annotation a : pa[i]) {
-                        if (a instanceof RequestParam) {
-                            String paramName = ((RequestParam)a).value();
-                            if (!"".equals(paramName.trim())) {
-                                String value = Arrays.toString(parameterMap.get(paramName))
-                                    .replaceAll("\\[|\\]", "")
-                                    .replaceAll("\\s", ",");
-                                paramValues[i] = value;
-                            }
-                        }
-                    }
-                }
             }
+            Integer index = handler.paramIndexMapping.get(param.getKey());
+            paramValues[index] = this.convert(paramTypes[index], value);
         }
-        String beanName = toLowerFirstCase(method.getDeclaringClass().getSimpleName());
-        method.invoke(this.ioc.get(method.getDeclaringClass().getName()),
-            new Object[] {req, resp, params.get("name")[0]});
+
+        if (handler.paramIndexMapping.containsKey(HttpServletRequest.class.getName())) {
+            Integer reqIndex = handler.paramIndexMapping.get(HttpServletRequest.class.getName());
+            paramValues[reqIndex] = req;
+        }
+        if (handler.paramIndexMapping.containsKey(HttpServletResponse.class.getName())) {
+            Integer reqIndex = handler.paramIndexMapping.get(HttpServletResponse.class.getName());
+            paramValues[reqIndex] = resp;
+        }
+
+        Object returnValue = handler.method.invoke(handler.controller, paramValues);
+        if (returnValue == null || returnValue instanceof Void) {
+            return;
+        }
+        resp.getWriter().write(returnValue.toString());
+    }
+
+    private Object convert(Class<?> paramType, String value) {
+        if (Integer.class == paramType) {
+            return Integer.valueOf(value);
+        }
+        return value;
+    }
+
+    private Handler getHandler(HttpServletRequest req) {
+        if (handlerMapping == null) {
+            return null;
+        }
+
+        String url = req.getRequestURI();
+        String contextPath = req.getContextPath();
+        url = url.replace(contextPath, "").replaceAll("/+", "/");
+        for (Handler handler : handlerMapping) {
+           try {
+                Matcher matcher = handler.pattern.matcher(url);
+                if (!matcher.matches()) {
+                    continue;
+                }
+                return handler;
+           } catch (Exception e) {
+               throw e;
+           }
+        }
+        return null;
     }
 
     @Override
@@ -133,9 +158,9 @@ public class DispatcherServlet extends HttpServlet {
                     continue;
                 }
                 RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
-                String url = (baseUrl + "/" + requestMapping.value()).replaceAll("/+", "/");
-                handlerMapping.put(url, method);
-                System.out.println("Mapped " + url + "," + method);
+                String regex = (baseUrl + "/" + requestMapping.value()).replaceAll("/+", "/");
+                handlerMapping.add(new Handler(entry.getValue(), method, Pattern.compile(regex)));
+                System.out.println("Mapped " + regex + "," + method);
             }
         }
     }
@@ -229,6 +254,49 @@ public class DispatcherServlet extends HttpServlet {
                 String clazzName = scanPackage + "." + file.getName().replace(".class", "");
                 classNames.add(clazzName);
             }
+        }
+    }
+
+    private class Handler {
+        protected Object controller;
+        protected Method method;
+        protected Pattern pattern;
+        protected Map<String, Integer> paramIndexMapping;
+
+        public Handler() {
+        }
+
+        public Handler(Object controller, Method method, Pattern pattern) {
+            this.controller = controller;
+            this.method = method;
+            this.pattern = pattern;
+            this.paramIndexMapping = new HashMap<>();
+            putParamIndexMapping(method);
+        }
+
+        private void putParamIndexMapping(Method method) {
+            Annotation[][] pa = method.getParameterAnnotations();
+            for (int i = 0; i < pa.length; i++) {
+                for (Annotation a : pa[i]) {
+                    if (a instanceof RequestParam) {
+                        String paramName = ((RequestParam)a).value();
+                        if (!"".equals(paramName.trim())) {
+                            paramIndexMapping.put(paramName, i);
+                        }
+                    }
+                }
+            }
+            Class<?>[] paramsTypes = method.getParameterTypes();
+            for (int i = 0; i < paramsTypes.length; i++) {
+                Class<?> type = paramsTypes[i];
+                if (type == HttpServletRequest.class || type == HttpServletResponse.class) {
+                    paramIndexMapping.put(type.getName(), i);
+                }
+            }
+        }
+
+        public Class<?>[] getParamTypes() {
+            return method.getParameterTypes();
         }
     }
 }
--- src/main/java/com/lujian/mvc/test/TestUtility.java
+++ src/main/java/com/lujian/mvc/test/TestUtility.java
@@ -1,8 +1,16 @@
 package com.lujian.mvc.test;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.lujian.mvc.annotation.RequestParam;
+import org.junit.Test;
 
 /**
  * @author lj167323@alibaba-inc.com
@@ -19,6 +27,35 @@ public class TestUtility {
             }
         }
         System.out.println(map);
+    }
+
+    @Test
+    public void testMain1() {
+        Method[] methods = TestUtility.class.getMethods();
+        for (Method method : methods) {
+            Annotation[][] pa = method.getParameterAnnotations();
+            System.out.println("method parameterAnnotations: " + method.getName());
+            for (int i = 0; i < pa.length; i++) {
+                for (Annotation annotation : pa[i]) {
+                    System.out.println(String.format("index: %s, annotation: %s", i, annotation));
+                }
+            }
+            System.out.println();
+            System.out.println("method parameterTypes");
+            Class<?>[] paramsTypes = method.getParameterTypes();
+            for (int i = 0; i < paramsTypes.length; i++) {
+                System.out.println(String.format("index: %s, parameterType: %s", i, paramsTypes));
+            }
+        }
+    }
+
+    @Test
+    public void testMain2() {
+        System.out.println("[tom]".replaceAll("\\[|\\]", ""));
+    }
+
+    public void methodA(String d, @RequestParam String a, @RequestParam String b, String c) {
+
     }
 
 }
